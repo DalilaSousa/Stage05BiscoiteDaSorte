f64d855ae83a344f92d73ed270c265d5dcf08d0c
Dalila Inácio Galvão Sousa <82845566+DalilaSousa@users.noreply.github.com>
2024-01-24 22:33:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // variáveis
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
+const fortuneCookie = document.querySelector("#fortune-Cookie")
+const btnReset = document.querySelector("#btnReset")
+
 const mensagensSorte = [
     "A vida trará grandes surpresas para você!",
     "Seja ousado e tudo será possível.",
@@ -9,28 +12,32 @@ const mensagensSorte = [
     "A sorte acompanha os corajosos."
 ];
 
+fortuneCookie.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'Enter' && screen2.classList.contains('hide')){
+        handleTryClick()
+    } else if(e.key == 'Enter' && screen1.classList.contains('hide')){
+        handleResetClick()
+        }
+    }
+)
+
+function handleTryClick(event) {
+    toggleScreen();
+    abrirBiscoito();
+}
+
 // funções
 function abrirBiscoito() {    
-    toggleScreen();
-
-    // Escolhe uma mensagem aleatória
-    const mensagemSorte = mensagensSorte[Math.floor(Math.random() * mensagensSorte.length)];
-
-    // Exibe a mensagem e esconde o botão
-    mensagemBiscoito.textContent = mensagemSorte;
+    let allFortunes = mensagensSorte.length
+    let randomNumber = Math.floor(Math.random() * allFortunes)  
+    screen2.querySelector("h2").innerText = `${mensagensSorte[randomNumber]}`
 }
 
 function toggleScreen() {
     screen1.classList.toggle("hide")
     screen2.classList.toggle("hide")
-
-    const mensagensSorte = [
-        "A vida trará grandes surpresas para você!",
-        "Seja ousado e tudo será possível.",
-        "Sua persistência será recompensada.",
-        "Acredite nos seus sonhos.",
-        "A sorte acompanha os corajosos."
-    ];
 }
 
 function handleResetClick() {
